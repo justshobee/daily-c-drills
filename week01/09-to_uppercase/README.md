d160f8e7cfba2f85ee21d208e0f8932c16b9002b
shobeedev <moha.saoud22@gmail.com>
2026-05-18 18:58:22 +0100

    ex09

---
+++ week01/09-to_uppercase/README.md
@@ -0,0 +1,14 @@
+# to_uppercase
+
+**Assignment name :** to_uppercase  
+**Expected files :** to_uppercase.c  
+**Allowed functions:** write
+
+---
+
+Write a program that converts all lowercase letters in the first argument to uppercase and prints the result followed by newline.
+
+```
+$> ./to_uppercase 'Hello! 123' | cat -e
+HELLO! 123$
+```
---
+++ week01/09-to_uppercase/solution.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://github.com/justshobee>           / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/05/18 18:36:26 by shobeedev               shobee4ever      */
+/*   Updated: 2026/05/18 18:57:52 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	to_uppercase(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			ft_putchar(str[i] - 32);
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int size, char **str)
+{
+	if (size == 2)
+		to_uppercase(str[1]);
+	ft_putchar('\n');
+	return (0);
+}
