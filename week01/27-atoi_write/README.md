0d2d6cd7ca7a4178e371b623a5e53368777a443a
shobeedev <moha.saoud22@gmail.com>
2026-05-21 11:39:08 +0100

    ex27

---
+++ week01/27-atoi_write/README.md
@@ -0,0 +1,14 @@
+# atoi_write
+
+**Assignment name :** atoi_write  
+**Expected files :** atoi_write.c  
+**Allowed functions:** write
+
+---
+
+Write a program that converts the first argument string to an integer (handle leading spaces and optional + or -) and prints the integer followed by newline. If no arg, print 0 and newline.
+
+```
+$> ./atoi_write ' -123abc' | cat -e
+-123$
+```
---
+++ week01/27-atoi_write/solution.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://github.com/justshobee>           / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/05/21 11:21:53 by shobeedev               shobee4ever      */
+/*   Updated: 2026/05/21 11:38:35 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nbr)
+{
+	long	n;
+	long	div;
+
+	n = nbr;
+	div = 1;
+	if (n == 0)
+	{
+		ft_putchar('0');
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	while (div <= (n / 10))
+		div *= 10;
+	while (div > 0)
+	{
+		ft_putchar((n / div) + '0');
+		n = n % div;
+		div = div / 10;
+	}
+}
+
+int		ft_atoi(char *str)
+{
+	int		res;
+	int		sign;
+	int		i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -sign;
+		i++;
+	}
+	if (str[i] == '+')
+		i++;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			res = res * 10 + (str[i] - '0');
+		else
+			break ;
+		i++;
+	}
+	return (res * sign);
+}
+
+int		main(int size, char **str)
+{
+	if (size == 2)
+		ft_putnbr(ft_atoi(str[1]));
+	ft_putchar('\n');
+	return (0);
+}
